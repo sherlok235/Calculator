4e95b06bff11caf8b960b8d860f3b8f6eee38ddd
sherlok235 <59393392+sherlok235@users.noreply.github.com>
2024-01-25 13:53:18 +0200

    delete some space

--- expresion.h
+++ expresion.h
@@ -12,17 +12,17 @@ private:
     double number1,number2;
     Parser pars;
 public:
-      expresion();
-      expresion(string input);
-      expresion(double num1,double num2);
-      expresion(double num1,Sign oper,double num2);
+    expresion();
+    expresion(string input);
+    expresion(double num1,double num2);
+    expresion(double num1,Sign oper,double num2);
 
-      void setOperation(Sign oper);
-      void setOperation(string temp);
+    void setOperation(Sign oper);
+    void setOperation(string temp);
 
-      double GetResult();
+    double GetResult();
 
-      void operator= (const string & input);
+    void operator= (const string & input);
 
 };
 
